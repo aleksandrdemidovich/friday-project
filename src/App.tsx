ce966f7e6e331bf85141fd276eb1f8297472a109
Valerian Ehret <66052889+valerianEhret@users.noreply.github.com>
2021-12-24 11:34:53 +0100

    Add isInitialized to App.tsx (#19)

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import React, {useEffect} from 'react';
 import './App.css';
 import Routes from "./pages/Routes";
 import {HashRouter} from "react-router-dom";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {initializingTC} from "./redux/authReducer";
+import {AppStateType} from "./redux/store";
+import {Preloader} from "./components/common/Preloader/Preloader";
+
+
 
 
 
@@ -11,11 +15,21 @@ import {initializingTC} from "./redux/authReducer";
 
 
 function App() {
+
+    const isInitialized = useSelector<AppStateType, boolean>(state=>state.app.isInitialized)
+    const isLoggedIn = useSelector<AppStateType, boolean>(state=>state.auth.isLoggedIn)
+
     const dispatch = useDispatch()
 
     useEffect( ()=>{
-        dispatch(initializingTC())
-    })
+        if (!isLoggedIn) {
+            dispatch(initializingTC())
+        }
+    },[dispatch])
+
+    if (!isInitialized) {
+        return <Preloader />
+    }
 
 
     return (
